e966b7fdcf080b5636e3d4e272362ae512543551
Fernando B <17830527+FernandoBoza@users.noreply.github.com>
2024-02-20 18:18:29 -0500

    Refactored file writing logic to media.service.ts

--- backend/src/routes/media.route.ts
+++ backend/src/routes/media.route.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
-import { getFileFormat, getFileName } from "../../../utils";
+import { writeFiles } from "../services/media.service";
 
 const media = new Hono();
 
@@ -23,47 +23,11 @@ media.get("/id/:id", (c) => {
 
 media.post("/upload", async (c) => {
   const files = await c.req.parseBody();
-  const uploadPath = "./uploads";
-
-  if (!files) return c.text("No files were uploaded");
-
-  const filesArray = Object.keys(files).map((fileName) => {
-    const file = files[fileName];
-
-    if (file instanceof File) {
-      return {
-        name: fileName,
-        data: file,
-        type: getFileFormat(file.type),
-      };
-    }
-
-    return {
-      name: fileName,
-      data: file,
-      type: "unknown",
-    };
-  });
-
-  for (const file of filesArray) {
-    const filePath = `${uploadPath}/${file.name}`;
-
-    if (await Bun.file(filePath).exists()) {
-      filesArray.splice(filesArray.indexOf(file), 1);
-      return c.text(`${getFileName(file.name)} already exists`);
-    }
-
-    await Bun.write(filePath, file.data);
+  try {
+    return await writeFiles(files, c);
+  } catch (error) {
+    return c.text("Error uploading files");
   }
-
-  return c.json(
-    filesArray.map((file) => {
-      return {
-        name: file.name,
-        type: file.type,
-      };
-    }),
-  );
 });
 
 export default media;
---
+++ backend/src/services/media.service.ts
@@ -0,0 +1,69 @@
+// @ts-ignore
+import type { BodyData } from "hono/dist/types/utils/body";
+import type { Context, Env } from "hono";
+import type { BlankInput } from "hono/types";
+import { getFileFormat, getFileName } from "../../../utils";
+
+// async function listFilesInDirectory(dirPath: string) {
+//   try {
+//     // The readdir method returns a Promise<string[]> indicating an array of file names
+//     const files: string[] = await Bun.(dirPath);
+//     console.log("Files and directories in:", dirPath);
+//     files.forEach((file: string) => {
+//       console.log(file);
+//     });
+//   } catch (error) {
+//     console.error("Error listing files in directory:", error);
+//   }
+// }
+//
+// // Example usage
+// const directoryPath: string = "./path/to/your/directory";
+// listFilesInDirectory(directoryPath);
+//
+// const files = await readdir(import.meta.dir);
+
+export const writeFiles = async (
+  files: BodyData,
+  ctx: Context<Env, "/upload", BlankInput>,
+) => {
+  const uploadPath = "./uploads";
+
+  if (!files) return ctx.text("No files were uploaded");
+
+  const filesArray = Object.keys(files).map((fileName) => {
+    const file = files[fileName];
+
+    if (file instanceof File) {
+      return {
+        name: fileName,
+        data: file,
+        type: getFileFormat(file.type),
+      };
+    }
+
+    return {
+      name: fileName,
+      data: file,
+      type: "unknown",
+    };
+  });
+
+  for await (const file of filesArray) {
+    const filePath = `${uploadPath}/${file.name}`;
+
+    if (await Bun.file(filePath).exists()) {
+      filesArray.splice(filesArray.indexOf(file), 1);
+      return ctx.text(`${getFileName(file.name)} already exists`);
+    }
+
+    await Bun.write(filePath, file.data);
+  }
+
+  return ctx.json(
+    filesArray.map((file) => ({
+      name: file.name,
+      type: file.type,
+    })),
+  );
+};
