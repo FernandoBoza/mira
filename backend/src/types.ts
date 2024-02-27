6afcd8efca93189de97ba57ae37866b9f4b1356d
fernandoboza <hi@fernandoboza.com>
2024-02-27 16:40:22 -0500

    Clean up of types.ts

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -1,15 +1,7 @@
 import type { Context, Env } from "hono";
-// @ts-ignore
-import type { BodyData } from "hono/dist/types/utils/body";
 import type { BlankInput } from "hono/types";
-import { API_UPLOAD_ENDPOINT } from "../../utils/constants.ts";
 
 export type VideoTypes = "mp4" | "webm" | "ogg" | "mkv" | "avi" | "mov";
-
-export type WriteFilesTypes = {
-  files: BodyData;
-  c: Context<Env, typeof API_UPLOAD_ENDPOINT, BlankInput>;
-};
 
 export type CustomFileType = {
   name: string;
