682e087b115efd70ef76f269c57aedcf8dc7f039
fernandoboza <hi@fernandoboza.com>
2024-03-03 21:58:42 -0500

    2 rows  First row split with horizontal columns and 2nd row below

--- src/pages/Editor.page.tsx
+++ src/pages/Editor.page.tsx
@@ -3,26 +3,27 @@ import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/componen
 export const EditorPage = () => {
   return (
     (
-      <ResizablePanelGroup direction="horizontal" className="border">
-        <ResizablePanel defaultSize={25}>
-          <div className="flex h-[91vh] h-xl:h-[93vh] items-center justify-center p-6"><span
-            className="font-semibold">One</span></div>
-        </ResizablePanel>
-        <ResizableHandle withHandle />
+      <ResizablePanelGroup direction="vertical" className="border">
         <ResizablePanel defaultSize={75}>
-          <ResizablePanelGroup direction="vertical">
+          <ResizablePanelGroup direction="horizontal">
+            <ResizablePanel defaultSize={25}>
+              <div className="flex h-full items-center justify-center p-6">
+                <span className="font-semibold">One</span>
+              </div>
+            </ResizablePanel>
+            <ResizableHandle withHandle />
             <ResizablePanel defaultSize={75}>
               <div className="flex h-full items-center justify-center p-6">
                 <span className="font-semibold">Two</span>
               </div>
             </ResizablePanel>
-            <ResizableHandle withHandle />
-            <ResizablePanel defaultSize={25}>
-              <div className="flex h-full items-center justify-center p-6">
-                <span className="font-semibold">Three</span>
-              </div>
-            </ResizablePanel>
           </ResizablePanelGroup>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={50}>
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">Three</span>
+          </div>
         </ResizablePanel>
       </ResizablePanelGroup>
     )
