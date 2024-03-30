8b58426c8ff6152d11c2320826e197cb983c5295
fernandoboza <hi@fernandoboza.com>
2024-03-29 23:35:21 -0400

    Working on generating scrub preview frames

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,23 +1,59 @@
-import { DragEvent, useCallback, useEffect, useRef } from 'react';
+import { DragEvent, useCallback, useRef, useState } from 'react';
 import { useFileStore } from '@/stores/file.store.ts';
+// import { FFmpeg } from '@ffmpeg/ffmpeg';
+// import { fetchFile } from '@ffmpeg/util';
 
 type TimelineProps = {
   duration: number;
   onScrub: (time: number) => void;
 };
 
+// const ffmpeg = new FFmpeg();
+
 export const Timeline = ({ duration, onScrub }: TimelineProps) => {
   const timelineRef = useRef<HTMLDivElement>(null);
   const { draggedFile, setDraggedFile } = useFileStore();
+  const [frames, setFrames] = useState<string[]>([]);
 
-  useEffect(() => {
-    // TODO: Extract frames from the video and display them in the timeline
-  }, [duration]);
+  //const ffmpeg = new FFmpeg();
+  // await ffmpeg.load();
+  // await ffmpeg.writeFile("video.avi", ...);
+  // // ffmpeg -i video.avi video.mp4
+  // await ffmpeg.exec([
+  // "-i",
+  // "video.avi",
+  // "video.mp4"
+  // ]);
+  // const data = ffmpeg.readFile("video.mp4");
 
-  const addTrack = (track: File) => {
-    console.log(track);
-    setDraggedFile(undefined);
-  };
+  // const commands = ['-i', 'output.webm', '-vf', 'fps=1', 'scale=-1:480', 'output_%d.jpg'];
+  //
+  // useEffect(() => {
+  //   const doTranscode = async () => {
+  //     await ffmpeg.load();
+  //     const file = await fetchFile(draggedFile);
+  //     await ffmpeg.writeFile('test.mp4', file);
+  //     await ffmpeg.exec(commands, -1);
+  //     const data = await ffmpeg.listDir('/');
+  //     const jpgFiles = data.filter((file) => file.name.endsWith('.jpg'));
+  //     const frames = jpgFiles.map((file) => {
+  //       const data = ffmpeg.readFile('file');
+  //       const base64 = Buffer.from(data).toString('base64');
+  //       return `data:image/jpg;base64,${base64}`;
+  //     });
+  //     setFrames(frames);
+  //   };
+  //   doTranscode().then((r) => console.log(r));
+  // }, [draggedFile, setFrames]);
+
+  const generateFrames = useCallback(
+    (track: File) => {
+      console.log(track);
+
+      setDraggedFile(undefined);
+    },
+    [setDraggedFile],
+  );
 
   const handleScrub = (event: React.MouseEvent) => {
     if (timelineRef.current) {
@@ -36,9 +72,9 @@ export const Timeline = ({ duration, onScrub }: TimelineProps) => {
   const handleDrop = useCallback(
     (e: DragEvent<HTMLDivElement>) => {
       preventDefaults(e);
-      if (draggedFile) addTrack(draggedFile);
+      if (draggedFile) generateFrames(draggedFile);
     },
-    [draggedFile, preventDefaults],
+    [generateFrames, draggedFile, preventDefaults],
   );
 
   return (
@@ -51,6 +87,9 @@ export const Timeline = ({ duration, onScrub }: TimelineProps) => {
       className="h-full"
     >
       TimeLine
+      {frames &&
+        frames.length > 0 &&
+        frames.map((frame, index) => <img key={index} src={frame} alt="scrub" />)}
     </div>
   );
 };
--- src/web.worker.ts
+++ src/web.worker.ts
@@ -1,5 +1,9 @@
 onmessage = function (e) {
   console.log(`${e.data} received from main script`);
+
+  // Test for multithread worker
+  const sab = new SharedArrayBuffer(1024);
+  postMessage(sab);
   postMessage('Sending message back to main script');
 };
 
