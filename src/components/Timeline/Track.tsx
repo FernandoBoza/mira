f743b8ce9a811ec97832a561a2328dcf3fd0e9a1
fernandoboza <hi@fernandoboza.com>
2024-04-09 19:19:11 -0400

    Separating track and timeline logic

---
+++ src/components/Timeline/Track.tsx
@@ -0,0 +1,152 @@
+import { Progress } from '@/components/ui/progress.tsx';
+import { ScrubTracker } from '@/components/Timeline/ScrubTracker.tsx';
+import {
+  Dispatch,
+  DragEvent,
+  MouseEvent,
+  MutableRefObject,
+  SetStateAction,
+  useCallback,
+  useRef,
+  useState,
+} from 'react';
+import { convertTime } from '@/lib/utils.ts';
+import { useEditorStore } from '@/stores/editor.store.ts';
+
+type TrackProps = {
+  loading: boolean;
+  displayedFrames: string[];
+  selectFile?: (file?: File) => void;
+  setLoading: (loading: boolean) => void;
+  setFrames: Dispatch<SetStateAction<string[]>>;
+  timelineRef: MutableRefObject<HTMLDivElement | null>;
+};
+
+export const Track = ({
+  loading,
+  displayedFrames,
+  selectFile,
+  setLoading,
+  setFrames,
+  timelineRef,
+}: TrackProps) => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  const { draggedFile, setDraggedFile, setTimeStamp } = useEditorStore();
+  const [isScrubbing, setIsScrubbing] = useState(false);
+  const [hoverTime, setHoverTime] = useState(0);
+  const [progress, setProgress] = useState(0);
+
+  const preventDefaults = useCallback((e: DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+  }, []);
+
+  const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
+    if (!videoRef?.current || !timelineRef?.current || !isScrubbing) return;
+    const timelineWidth = timelineRef.current.offsetWidth;
+    const clickPosition = e.clientX - timelineRef.current.getBoundingClientRect().left;
+    const time = (clickPosition / timelineWidth) * videoRef.current.duration;
+
+    setHoverTime(time);
+
+    if (isScrubbing) {
+      videoRef.current.currentTime = time;
+      if (time <= 0) {
+        setTimeStamp && setTimeStamp('00:00');
+        return;
+      }
+      setTimeStamp && setTimeStamp(convertTime(time));
+    }
+  };
+
+  const handleMouseDirection = (direction: 'up' | 'down') => {
+    setIsScrubbing(direction === 'down');
+  };
+
+  const handleDrop = useCallback(
+    (e: DragEvent<HTMLDivElement>) => {
+      preventDefaults(e);
+      e.stopPropagation();
+
+      const generateFramesFromVideo = (video: HTMLVideoElement) => {
+        const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+        const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+        const { width, height } = canvas;
+        const totalFrames = Math.floor(video.duration / 2); // half of video duration
+        let currentFrame = 0;
+
+        const generateFrame = () => {
+          const currentProgress = Math.floor((currentFrame / totalFrames) * 100);
+          setProgress(currentProgress);
+          if (currentFrame >= totalFrames) {
+            setLoading(false);
+            video.onseeked = null; // Remove the event listener
+            return;
+          } else {
+            setLoading(true);
+            video.currentTime = currentFrame * 2; // Capture a frame every 2 seconds
+            video.onseeked = () => {
+              ctx.drawImage(video, 0, 0, width, height);
+              const data = canvas.toDataURL('image/jpeg');
+              currentFrame++;
+              requestAnimationFrame(generateFrame);
+              setFrames((prevFrames) => [...prevFrames, data]);
+            };
+          }
+        };
+
+        video.currentTime = 0;
+        requestAnimationFrame(generateFrame);
+      };
+
+      if (draggedFile) {
+        selectFile && selectFile(draggedFile);
+
+        const url = URL.createObjectURL(draggedFile);
+        const video = document.createElement('video');
+
+        videoRef.current = video;
+        video.onloadedmetadata = () => {
+          generateFramesFromVideo(video);
+        };
+        video.src = url;
+        setDraggedFile(undefined);
+      }
+    },
+    [preventDefaults, draggedFile, setLoading, setFrames, selectFile, videoRef, setDraggedFile],
+  );
+
+  return (
+    <div
+      ref={timelineRef}
+      onClick={handleMouseMove}
+      onMouseMove={handleMouseMove}
+      onMouseDown={() => handleMouseDirection('down')}
+      onMouseUp={() => handleMouseDirection('up')}
+      onDragEnter={preventDefaults}
+      onDragOver={preventDefaults}
+      onDrop={handleDrop}
+      className="h-20 relative border border-primary rounded-lg overflow-hidden"
+      id="timeline"
+    >
+      {loading && (
+        <div className="p-6">
+          <Progress value={progress} />
+        </div>
+      )}
+      <canvas id="canvas" width="500" height="300" className="hidden"></canvas>
+      <div
+        id="frameContainer"
+        className="h-full flex overflow-x-scroll select-none pointer-events-none"
+      >
+        {displayedFrames.map((frame, index) => (
+          <img key={index} src={frame} className="pointer-events-none select-none" alt="frame" />
+        ))}
+        {progress === 100 && (
+          <ScrubTracker hoverTime={hoverTime} duration={videoRef?.current?.duration as number} />
+        )}
+      </div>
+    </div>
+  );
+};
--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,26 +1,17 @@
-import { DragEvent, MouseEvent, useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { useEditorStore } from '@/stores/editor.store.ts';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { Slider } from '../ui/slider';
-import { convertTime } from '@/lib/utils.ts';
-import { Progress } from '@/components/ui/progress.tsx';
-import { ScrubTracker } from '@/components/Timeline/ScrubTracker.tsx';
+import { Track } from '@/components/Timeline/Track.tsx';
 
-type TimelineProps = {
-  selectFile?: (file?: File) => void;
-};
+type TimelineProps = { selectFile?: (file?: File) => void };
 
 export const Timeline = ({ selectFile }: TimelineProps) => {
-  const { draggedFile, setDraggedFile, setTimeStamp } = useEditorStore();
   const timelineRef = useRef<HTMLDivElement | null>(null);
   const containerRef = useRef<HTMLDivElement | null>(null);
-  const videoRef = useRef<HTMLVideoElement | null>(null);
+
   const [frames, setFrames] = useState<string[]>([]);
   const [displayedFrames, setDisplayedFrames] = useState<string[]>([]);
   const [loading, setLoading] = useState(false);
-  const [isScrubbing, setIsScrubbing] = useState(false);
-  const [hoverTime, setHoverTime] = useState(0);
   const [sliderValue, setSliderValue] = useState(10);
-  const [progress, setProgress] = useState(0);
   const [scale, setScale] = useState(10);
 
   const steps = useMemo(() => {
@@ -32,14 +23,17 @@ export const Timeline = ({ selectFile }: TimelineProps) => {
   const handleSliderChange = useCallback(
     (value: number[]) => {
       if (!frames.length) return;
-      const sliderValue = value[0];
-      setSliderValue(sliderValue);
-      const framesAtIntervals = Array.from({ length: steps }, (_, i) =>
-        Math.floor((i * frames.length) / steps),
+      const newSliderValue = value[0];
+      let newSteps = Math.floor((newSliderValue / 100) * frames.length);
+      if (newSteps < 10) newSteps = 10;
+      const framesAtIntervals = Array.from({ length: newSteps }, (_, i) =>
+        Math.floor((i * frames.length) / newSteps),
       ).map((index) => frames[index]);
+
+      setSliderValue(newSliderValue);
       setDisplayedFrames(framesAtIntervals);
     },
-    [frames, steps],
+    [frames],
   );
 
   useEffect(() => {
@@ -71,85 +65,6 @@ export const Timeline = ({ selectFile }: TimelineProps) => {
     };
   }, [handleSliderChange, scale, sliderValue]);
 
-  const generateFramesFromVideo = (video: HTMLVideoElement) => {
-    const canvas = document.getElementById('canvas') as HTMLCanvasElement;
-    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
-    const { width, height } = canvas;
-    const totalFrames = Math.floor(video.duration / 2); // half of video duration
-    let currentFrame = 0;
-
-    const generateFrame = () => {
-      const currentProgress = Math.floor((currentFrame / totalFrames) * 100);
-      setProgress(currentProgress);
-      if (currentFrame >= totalFrames) {
-        setLoading(false);
-        video.onseeked = null; // Remove the event listener
-        return;
-      } else {
-        setLoading(true);
-        video.currentTime = currentFrame * 2; // Capture a frame every 2 seconds
-        video.onseeked = () => {
-          ctx.drawImage(video, 0, 0, width, height);
-          const data = canvas.toDataURL('image/jpeg');
-          currentFrame++;
-          requestAnimationFrame(generateFrame);
-          setFrames((prevFrames) => [...prevFrames, data]);
-        };
-      }
-    };
-
-    video.currentTime = 0;
-    requestAnimationFrame(generateFrame);
-  };
-
-  const preventDefaults = useCallback((e: DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    e.stopPropagation();
-  }, []);
-
-  const handleDrop = useCallback(
-    (e: DragEvent<HTMLDivElement>) => {
-      preventDefaults(e);
-      e.stopPropagation();
-      if (draggedFile) {
-        selectFile && selectFile(draggedFile);
-
-        const url = URL.createObjectURL(draggedFile);
-        const video = document.createElement('video');
-
-        videoRef.current = video;
-        video.onloadedmetadata = () => {
-          generateFramesFromVideo(video);
-        };
-        video.src = url;
-        setDraggedFile(undefined);
-      }
-    },
-    [preventDefaults, draggedFile, selectFile, setDraggedFile],
-  );
-
-  const handleMouseMove = (e: MouseEvent<HTMLDivElement>) => {
-    if (!videoRef?.current || !timelineRef?.current || !isScrubbing) return;
-    const timelineWidth = timelineRef.current.offsetWidth;
-    const clickPosition = e.clientX - timelineRef.current.getBoundingClientRect().left;
-    const time = (clickPosition / timelineWidth) * videoRef.current.duration;
-
-    setHoverTime(time);
-
-    if (isScrubbing) {
-      videoRef.current.currentTime = time;
-      if (time <= 0) {
-        setTimeStamp && setTimeStamp('00:00');
-        return;
-      }
-      setTimeStamp && setTimeStamp(convertTime(time));
-    }
-  };
-
-  const handleMouseDirection = (direction: 'up' | 'down') => {
-    setIsScrubbing(direction === 'down');
-  };
-
   useEffect(() => {
     if (loading || (!loading && frames.length && displayedFrames.length)) {
       return;
@@ -175,36 +90,14 @@ export const Timeline = ({ selectFile }: TimelineProps) => {
         />
         <h5>frames on track: {displayedFrames.length}</h5>
       </div>
-      <div
-        ref={timelineRef}
-        onClick={handleMouseMove}
-        onMouseMove={handleMouseMove}
-        onMouseDown={() => handleMouseDirection('down')}
-        onMouseUp={() => handleMouseDirection('up')}
-        onDragEnter={preventDefaults}
-        onDragOver={preventDefaults}
-        onDrop={handleDrop}
-        className="h-1/3 relative border border-primary rounded-lg overflow-hidden"
-        id="timeline"
-      >
-        {loading && (
-          <div className="p-6">
-            <Progress value={progress} />
-          </div>
-        )}
-        <canvas id="canvas" width="500" height="300" className="hidden"></canvas>
-        <div
-          id="frameContainer"
-          className="h-full flex overflow-x-scroll select-none pointer-events-none"
-        >
-          {displayedFrames.map((frame, index) => (
-            <img key={index} src={frame} className="pointer-events-none select-none" alt="frame" />
-          ))}
-          {progress === 100 && (
-            <ScrubTracker hoverTime={hoverTime} duration={videoRef?.current?.duration as number} />
-          )}
-        </div>
-      </div>
+      <Track
+        loading={loading}
+        setFrames={setFrames}
+        selectFile={selectFile}
+        setLoading={setLoading}
+        timelineRef={timelineRef}
+        displayedFrames={displayedFrames}
+      />
     </div>
   );
 };
